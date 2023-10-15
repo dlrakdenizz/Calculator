9227bebf4effa1b15f5ab4442cb067dbc8d6296e
Dilara Akdeniz <dilarakdeniz15013@gmail.com>
2023-10-15 12:14:10 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dilara Akdeniz on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
